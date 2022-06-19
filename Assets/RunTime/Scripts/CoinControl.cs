5aa0a0e45481df861643fad88307c6a175461b89
Dfam92 <dfam92@hotmail.com>
2022-06-19 11:53:18 -0300

    Coin Collect and Movements

--- Assets/RunTime/Scripts/CoinControl.cs
+++ Assets/RunTime/Scripts/CoinControl.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CoinControl : MonoBehaviour
 {
     [SerializeField] float rotationSpeed;
     [SerializeField] PlayerControl player;
-
+    [SerializeField] GameManager gameManager;
+    [SerializeField] GameObject coinUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +18,19 @@ public class CoinControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+        this.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
+            gameManager.SumCoins();
             player.CoinCollision();
-            this.gameObject.SetActive(false);
+            this.gameObject.GetComponent<Collider>().enabled = false;
+            this.transform.DOLocalMove(Camera.main.ViewportToWorldPoint(coinUI.transform.position), 9);
+            Destroy(this.gameObject, 10);
+            
         }
     }
 }
--- Assets/RunTime/Scripts/GameManager.cs
+++ Assets/RunTime/Scripts/GameManager.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI coinValue;
+    private int coinsCollected;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinValue.text = " ";
     }
 
     // Update is called once per frame
@@ -17,6 +22,11 @@ public class GameManager : MonoBehaviour
         
     }
 
+    public void SumCoins()
+    {
+        coinsCollected += 1;
+        coinValue.text = " " + coinsCollected;
+    }
     public void RestartScene()
     {
         SceneManager.LoadScene("WaterRush");
